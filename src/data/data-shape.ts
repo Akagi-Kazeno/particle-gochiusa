326f4a0a3401cde4c95c864cacd423fe84fc628d
Akagi-Kazeno <akagikazeno@gmail.com>
2023-05-29 15:02:29 +0800

    shape

---
+++ src/data/data-shape.ts
@@ -0,0 +1,37 @@
+// CONCATENATED MODULE: ./src/data/data-shape.ts
+/**
+ * Created by 「asset-shapes.fla」/「generate-assets.jsfl」 on Wed Jan 20 2016
+ * !!!!!このコードはJSFLから自動生成されたコードです。修正する場合はご注意ください。!!!!!
+ * !!!!!这段代码是JSFL自动生成的代码。修改的时候请注意。!!!!!
+ */
+class ShapeData {
+    assetList: string[];
+
+    constructor() {
+        this.assetList = ["blur_circle",
+            "circle",
+            "flower",
+            "heart",
+            "kirakira",
+            "kirakira2",
+            "reverse_blur_circle",
+            "square",
+            "star",
+            "star_10",
+            "triangle",
+            "sakura1",
+            "sakura2",
+            "sakura3",
+            "sakura4",
+            "sakura5",
+            "sakura6",
+            "sakura7",
+            "sakura8",
+            "sakura9",
+            "sakura10",
+            "sakura11",
+            "sakura12",
+            "sakura13"
+        ];
+    }
+}
