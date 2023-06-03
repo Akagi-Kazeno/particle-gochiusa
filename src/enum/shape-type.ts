a3c9b80919e1a49ffaea43404d4ad4c584a17fca
Akagi-Kazeno <akagikazeno@gmail.com>
2023-06-03 15:31:04 +0800

    enums

---
+++ src/enum/shape-type.ts
@@ -0,0 +1,90 @@
+/**
+ * シェイプの種類を定義したクラスです。
+ * 塑形的种类的类别
+ */
+class ShapeType {
+    /**
+     * ふわっとした円のシェイプIDです。
+     * @type {string}
+     * 模糊的圆的ID
+     */
+    static readonly BLUR_CIRCLE = "blur_circle";
+    /**
+     * 円のシェイプIDです。
+     * @type {string}
+     * 圆形的ID
+     */
+    static readonly CIRCLE = "circle";
+    /**
+     * 花の形のシェイプIDです。
+     * @type {string}
+     * 花形状ID
+     */
+    static readonly FLOWER = "flower";
+    /**
+     * ハートの形のシェイプIDです。
+     * @type {string}
+     * 心形ID
+     */
+    static readonly HEART = "heart";
+    /**
+     * キラキラ(1)のシェイプIDです。
+     * 闪闪发光(1)(?)的形状ID
+     * @type {string}
+     */
+    static readonly KIRAKIRA = "kirakira"
+    /**
+     * キラキラ(2)のシェイプIDです。
+     * 闪闪发光(2)(?)的形状ID
+     * @type {string}
+     */
+    static readonly KIRAKIRA2 = "kirakira2"
+    /**
+     * 中央が繰り抜かれた円のシェイプIDです。
+     * @type {string}
+     * 中间挖空模糊的圆的形状(?)的形状ID
+     */
+    static readonly REVERSE_CIRCLE = "reverse_blur_circle";
+    /**
+     * 四角形のシェイプIDです。
+     * 方形ID
+     * @type {string}
+     */
+    static readonly SQUARE = "square";
+    /**
+     * 星形のシェイプIDです。
+     * @type {string}
+     * 星形ID
+     */
+    static readonly STAR = "star";
+    /**
+     * 星形(棘が10)のシェイプIDです。
+     * @type {string}
+     * 十芒星ID
+     */
+    static readonly STAR_10 = "star_10";
+    /**
+     * 三角形のシェイプIDです。
+     * @type {string}
+     * 三角形ID
+     */
+    static readonly TRIANGLE = "TRIANGLE";
+    /**
+     * 泡のシェイプIDです。
+     * @type {string}
+     * 泡沫形状ID
+     */
+    static readonly SAKURA1 = "sakura1";
+    static readonly SAKURA2 = "sakura2";
+    static readonly SAKURA3 = "sakura3";
+    static readonly SAKURA4 = "sakura4";
+    static readonly SAKURA5 = "sakura5";
+    static readonly SAKURA6 = "sakura6";
+    static readonly SAKURA7 = "sakura7";
+    static readonly SAKURA8 = "sakura8";
+    static readonly SAKURA9 = "sakura9";
+    static readonly SAKURA10 = "sakura10";
+    static readonly SAKURA11 = "sakura11";
+    static readonly SAKURA12 = "sakura12";
+    static readonly SAKURA13 = "sakura13";
+}
