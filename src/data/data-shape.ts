4eb1d77a9de9cea56d6d7e99a1d0a1faf710c7f0
Akagi-Kazeno <akagikazeno@gmail.com>
2023-06-04 11:04:33 +0800

    data

---
+++ src/data/data-shape.ts
@@ -0,0 +1,31 @@
+class ShapeData {
+    assetList:string[];
+    constructor() {
+        this.assetList = [
+            "blur_circle",
+            "circle",
+            "flower",
+            "heart",
+            "kirakira",
+            "kirakira2",
+            "reverse_blur_circle",
+            "square",
+            "star",
+            "star_10",
+            "triangle",
+            "sakura1",
+            "sakura2",
+            "sakura3",
+            "sakura4",
+            "sakura5",
+            "sakura6",
+            "sakura7",
+            "sakura8",
+            "sakura9",
+            "sakura10",
+            "sakura11",
+            "sakura12",
+            "sakura13"
+        ];
+    }
+}
