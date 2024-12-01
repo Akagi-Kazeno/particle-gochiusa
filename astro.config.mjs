85d9257a4e5dfc11df8986d4c030d9031ce6ca57
Akagi-Kazeno <akagikazeno@gmail.com>
2024-12-01 22:00:54 +0800

    feat(framework): init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "particles",
+      social: {
+        github: "https://github.com/Akagi-Kazeno/particle-gochiusa",
+      },
+      sidebar: [],
+      customCss: ['./src/tailwind.css'],
+    }),
+    tailwind({applyBaseStyles: false}),
+  ],
+});
---
+++ src/config/sidebar.ts
@@ -0,0 +1,8 @@
+import type {SidebarItem} from "node_modules/@astrojs/starlight/schemas/sidebar";
+
+export const sidebar: SidebarItem[] = [
+  {label: '首页', link: '/', translations: {}, attrs: {}},
+  {label: 'example', link: '/example/', translations: {}, attrs: {}},
+  {label: 'gochiusa', link: '/gochiusa/common/', translations: {}, attrs: {}},
+  {label: 'gochiusa-10th', link: '/gochiusa/10th/', translations: {}, attrs: {}},
+];
