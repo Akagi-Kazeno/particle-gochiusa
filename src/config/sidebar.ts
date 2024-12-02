1c1f6cde6e416439185b9e006d84eedf0f4b448a
Akagi-Kazeno <akagikazeno@gmail.com>
2024-12-02 22:36:23 +0800

    feat(sakura): add sakura particle

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -5,4 +5,5 @@ export const sidebar: SidebarItem[] = [
   {label: 'example', link: '/example/', translations: {}, attrs: {}},
   {label: 'gochiusa', link: '/gochiusa/common/', translations: {}, attrs: {}},
   {label: 'gochiusa-10th-bg', link: '/gochiusa/10th/', translations: {}, attrs: {}},
+  {label: 'sakura', link: '/sakura/', translations: {}, attrs: {}},
 ];
